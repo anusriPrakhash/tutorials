muhrizky/tutorials-owl

[Solution] 1.7 A todo list

owl_playground/static/src/todo_list/todo_item.js
@@ -1,0 +1,13 @@
+/** @odoo-module */
+
+import { Component } from "@odoo/owl";
+
+export class TodoItem extends Component {
+    static template = "owl_playground.TodoItem";
+    static props = {
+        todo: {
+            type: Object,
+            shape: { id: Number, description: String, isCompleted: Boolean }
+        }
+    };
+}

owl_playground/static/src/todo_list/todo_list.js
@@ -1,0 +1,16 @@
+/** @odoo-module */
+
+import { Component, useState } from "@odoo/owl";
+import { TodoItem } from "./todo_item";
+
+export class TodoList extends Component {
+    static template = "owl_playground.TodoList";
+    static components = { TodoItem };
+
+    setup() {
+        this.todos = useState([
+            { id: 2, description: "write tutorial", isCompleted: true },
+            { id: 3, description: "buy milk", isCompleted: false },
+        ]);
+    }
+}

anusriPrakhash/tutorials

awesome_owl/static/src/todo_list/todo_item.js
@@ -1,0 +1,11 @@
+import { Component } from "@odoo/owl";
+
+export class TodoItem extends Component {
+    static template = "awesome_owl.TodoItem";
+    static props = {
+        todo: {
+            type: Object,
+            shape: { id: Number, description: String, isCompleted: Boolean }
+        }
+    };
+}

awesome_owl/static/src/todo_list/todo_list.js
@@ -1,0 +1,14 @@
+import { Component, useState } from "@odoo/owl";
+import { TodoItem } from "./todo_item";
+
+export class TodoList extends Component {
+    static template = "awesome_owl.TodoList";
+    static components = { TodoItem };
+
+    setup() {
+        this.todos = useState([
+            { id: 2, description: "write tutorial", isCompleted: true },
+            { id: 3, description: "buy milk", isCompleted: false },
+        ]);
+    }
+}
0c6132959db61a2576087d3b223a97a3d74c8e7b
Géry Debongnie <ged@odoo.com>
2023-05-10 15:50:22 +0200

    [Solution] 1.7 A todo list

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,7 @@
 import { Component, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
+import { TodoList } from "./todo_list/todo_list";
 
 class CounterModel {
     constructor(value) {
@@ -18,7 +19,7 @@ class CounterModel {
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter, Card };
+    static components = { Counter, Card, TodoList };
 
     setup() {
         this.counters = useState([
