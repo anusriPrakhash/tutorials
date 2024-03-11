b36686f7a0b0ce3558069b5346d069b63f1fcc64
anusriPrakhash <anusriprakhash@gmail.com>
2024-03-11 12:01:01 +0100

    Create a model estate

---
+++ estate/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Real Estate',
+    'version': '0.1',
+    'depends': ['base'],
+    'author': 'Odoo',
+    'category': 'Tutorials/RealEstate',
+    'application': True,
+    'installable': True,
+    'data': [],
+    'summary': """
+        Starting module for "Master the Odoo web framework, chapter 1: Real Estate"
+    """,
+
+    'description': """
+        Starting module for "Master the Odoo web framework, chapter 1: Real Estate.
+    """,
+    #'auto_install': False
+}
---
+++ estate/models/estate_property.py
@@ -0,0 +1,5 @@
+from odoo import models, fields
+
+class Property(models.Model):
+    _name = "estate_property"
+    _description = "Property of estate"
